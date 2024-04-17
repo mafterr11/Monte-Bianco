c920fdd0046fef68ef14a8887a48a9c0cf440069
mafterr11 <mafterr11@gmail.com>
2024-04-17 22:59:10 +0300

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,17 +4,17 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='bg-body-accent flex items-center justify-between px-[88px] py-[22px]'>
+    <footer className='bg-body-accent flex max-md:flex-col items-center justify-between max-md:gap-y-2 max-md:px-6 xl:px-[88px] py-[22px]'>
         {/* Left side */}
-        <div className='flex items-start justify-normal flex-col'>
+        <div className='flex items-start justify-normal max-md:flex-row max-md:gap-x-6 flex-col gap-y-2'>
             <Link href="/termeni-si-conditii">Termeni si Conditii</Link>
             <Link href="/contact">Contact</Link>
         </div>
         {/* Center */}
-        <div>Creat de <Link href="https://www.myriad-tech.ro/" className='hover hover:font-medium'><span>Myriad Tech</span></Link> @ 2024 Monte Bianco</div>
+        <div className='text-[13px]'>Creat de <Link href="https://www.myriad-tech.ro/" className='hover hover:font-medium'><span>Myriad Tech</span></Link> @ 2024 Monte Bianco</div>
         {/* Right side */}
         {/* Logo */}
-        <div className="inline-block">
+        <div className="inline-block max-md:hidden">
           <Link href="/">
             <Image
               src="/logo.png"

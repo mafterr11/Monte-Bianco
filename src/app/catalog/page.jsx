4996fee2f9a8fb808f49f1447c1f747eea323ccb
Alexandru Maftei <mafterr11@gmail.com>
2024-04-18 10:45:41 +0300

    .

---
+++ src/app/catalog/page.jsx
@@ -0,0 +1,11 @@
+"use client"
+import { Tabs } from '@/components/ui/tabs'
+import React from 'react'
+
+const Catalog = () => {
+  return (
+    <div><Tabs /></div>
+  )
+}
+
+export default Catalog

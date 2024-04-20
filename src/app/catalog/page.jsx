0c04f718ed90e71ec9f8ab635742e672d886d970
mafterr11 <mafterr11@gmail.com>
2024-04-20 16:13:07 +0300

    .

--- src/app/catalog/page.jsx
+++ src/app/catalog/page.jsx
@@ -5,49 +5,76 @@ import CardProdus from "@/components/pages/catalog/CardProdus";
 import { productData } from "@/products";
 
 const Catalog = () => {
-
+  // Categories setup
   const uniqueCategories = [
     "Toate produsele",
     ...new Set(productData.map((item) => item.category)),
   ];
-  const [categories, setCategories] = useState(uniqueCategories);
   const [category, setCategory] = useState("Toate produsele");
-  const filteredProducts = productData.filter((product) => {
-    // if category  is all products return all products, else filter
-    return category === "Toate produsele"
-      ? product
-      : product.category === category;
-  });
+
+  // Brands setup
+  const uniqueBrands = [...new Set(productData.map((item) => item.brand))];
+  const [brand, setBrand] = useState("All Brands"); // Using 'All Brands' as a default to show all
+
+  // Filter products by categories
+  const filteredByCategory = productData.filter(
+    (product) => category === "Toate produsele" || product.category === category
+  );
+
+  // Filter products by brands
+  const filteredByBrand = productData.filter(
+    (product) => brand === "All Brands" || product.brand === brand
+  );
 
   return (
-    <section className="min-h-screen pt-32 md:pt-40">
-      <div className="container mx-auto">
-        <h2 className="mb-8 xl:mb-32 text-center mx-auto">
+    <section className='min-h-screen pt-32 md:pt-40'>
+      <div className='container mx-auto'>
+        <h2 className='mb-8 xl:mb-32 text-center mx-auto'>
           Gama de produse Monte Bianco
         </h2>
         {/* tabs */}
-        <Tabs defaultValue={category} className="mb-24 xl:mb-48">
-          <TabsList className="w-full grid h-full grid-cols-1 md:grid-cols-3 lg:max-w-[940px] mb-12 mx-auto gap-2">
-            {categories.map((category, index) => {
-              return (
-                <TabsTrigger
-                  value={category}
-                  key={index}
-                  className="uppercase w-[300px] max-md:mx-auto md:w-auto border-[#dadada] border"
-                  onClick={() => setCategory(category)}
-                >
-                  {category}
-                </TabsTrigger>
-              );
-            })}
+        <Tabs defaultValue={category} className='mb-24 xl:mb-48'>
+          {/* Category Tabs */}
+          <TabsList className='w-full grid h-full grid-cols-1 md:grid-cols-3 lg:max-w-[940px] mb-12 mx-auto gap-2'>
+            {uniqueCategories.map((cat, index) => (
+              <TabsTrigger
+                value={cat}
+                key={index}
+                className='uppercase w-[300px] max-md:mx-auto md:w-auto border-[#dadada] border'
+                onClick={() => setCategory(cat)}
+              >
+                {cat}
+              </TabsTrigger>
+            ))}
           </TabsList>
-          {/* tabs content */}
-          <div className="text-lg xl:mt-12 grid grid-cols-1 lg:grid-cols-3 gap-4">
-            {filteredProducts.map((product, index)=>{
-              return <TabsContent value={category} key={index}>
-                <CardProdus product={product}/>
+          {/* Brand Tabs */}
+          <TabsList className='w-full grid grid-cols-1 md:grid-cols-4 lg:max-w-[540px] mb-12 mx-auto gap-2'>
+            {uniqueBrands.map((br, index) => (
+              <TabsTrigger
+                value={br}
+                key={index}
+                className='uppercase w-[300px] max-md:mx-auto md:w-auto border-[#dadada] border'
+                onClick={() => setBrand(br)}
+              >
+                {br}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+          {/* Category Cards Mapping */}
+          <div className='text-lg mt-40 xl:mt-12 grid grid-cols-1 lg:grid-cols-3 gap-4'>
+            {filteredByCategory.map((product, index) => (
+              <TabsContent value={category} key={index}>
+                <CardProdus product={product} />
               </TabsContent>
-            })}
+            ))}
+          </div>
+          {/* Brands Cards Mapping */}
+          <div className='text-lg xl:mt-12 grid grid-cols-1 lg:grid-cols-3 gap-4'>
+            {filteredByBrand.map((product, index) => (
+              <TabsContent value={brand} key={index}>
+                <CardProdus product={product} />
+              </TabsContent>
+            ))}
           </div>
         </Tabs>
       </div>

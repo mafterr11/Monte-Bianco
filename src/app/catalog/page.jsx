521f3d46e8d4d02a92008f5e72a8deec7ac15352
mafterr11 <mafterr11@gmail.com>
2024-04-21 10:33:35 +0300

    .

--- src/app/catalog/page.jsx
+++ src/app/catalog/page.jsx
@@ -37,7 +37,7 @@ const Catalog = () => {
   );
 
   return (
-    <section className='min-h-screen pt-32 md:mt-24'>
+    <section className='min-h-screen pt-32 md:mt-16'>
       <div className='container mx-auto'>
         <h2 className='mb-56 text-center mx-auto'>
           Gama de produse Monte Bianco

f76198d1d94be5e74f53d5833a1b224bc6070213
mafterr11 <mafterr11@gmail.com>
2024-04-22 20:06:29 +0300

    ..

--- src/app/catalog/page.jsx
+++ src/app/catalog/page.jsx
@@ -6,31 +6,31 @@ import { productData } from "@/products";
 import Image from "next/image";
 
 const Catalog = () => {
+  // Categories
   // Filter products that have categories defined
   const productsWithCategories = productData.filter(
     (product) => product.category
   );
-
   // Categories setup
   const uniqueCategories = [
     "Toate produsele",
     ...new Set(productsWithCategories.map((product) => product.category)),
   ];
   const [category, setCategory] = useState("Toate produsele");
+  // Filter products by categories
+  const filteredByCategory = productData.filter(
+    (product) => category === "Toate produsele" || product.category === category
+  );
 
-  // Filter products that have categories defined
+  
+  // Brands
+  // Filter products that have brands defined
   const productsWithBrands = productData.filter((product) => product.brand);
   // Brands setup
   const uniqueBrands = [
     ...new Set(productsWithBrands.map((product) => product.brand)),
   ];
   const [brand, setBrand] = useState("All Brands"); // Using 'All Brands' as a default to show all
-
-  // Filter products by categories
-  const filteredByCategory = productData.filter(
-    (product) => category === "Toate produsele" || product.category === category
-  );
-
   // Filter products by brands
   const filteredByBrand = productData.filter(
     (product) => brand === "All Brands" || product.brand === brand
@@ -46,7 +46,7 @@ const Catalog = () => {
         <Tabs defaultValue={category} className='mb-24 xl:mb-48'>
           {/* Category and brand Tabs */}
           <TabsList className='flex flex-col items-center justify-center gap-y-8 max-md:gap-y-48 max-md:mb-32'>
-            <div className="w-full grid h-full grid-cols-1 md:grid-cols-3 lg:max-w-[940px] mb-12 mx-auto gap-2 overflow">
+            <div className='w-full grid h-full grid-cols-1 md:grid-cols-3 lg:max-w-[940px] mb-12 mx-auto gap-2 overflow'>
               {uniqueCategories.map((cat, index) => (
                 <TabsTrigger
                   value={cat}
@@ -58,17 +58,17 @@ const Catalog = () => {
                 </TabsTrigger>
               ))}
             </div>
-            <div className="w-full grid h-full grid-cols-1 md:grid-cols-5 lg:max-w-[800px] mx-auto gap-2 overflow max-md:mt-6">
-            {uniqueBrands.map((br, index) => (
-              <TabsTrigger
-              value={br}
-              key={index}
-              className='uppercase w-[300px] max-md:mx-auto md:w-auto border-[#dadada] border data-[state=active]:bg-gradient-to-t from-accent/40 via-accent/20 to-body-accent/20'
-              onClick={() => setBrand(br)}
-              >
-                <Image src={br} width={120} height={60} alt={br}/>
-              </TabsTrigger>
-            ))}
+            <div className='w-full grid h-full grid-cols-1 md:grid-cols-5 lg:max-w-[800px] mx-auto gap-2 overflow max-md:mt-6'>
+              {uniqueBrands.map((br, index) => (
+                <TabsTrigger
+                  value={br}
+                  key={index}
+                  className='uppercase w-[300px] max-md:mx-auto md:w-auto border-[#dadada] border data-[state=active]:bg-gradient-to-t from-accent/40 via-accent/20 to-body-accent/20'
+                  onClick={() => setBrand(br)}
+                >
+                  <Image src={br} width={120} height={60} alt={br} />
+                </TabsTrigger>
+              ))}
             </div>
           </TabsList>
           {/* Category Cards Mapping */}

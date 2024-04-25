2b934b1fcfe2702deff7289ee0c6217db9c6a904
Alexandru Maftei <mafterr11@gmail.com>
2024-04-25 11:44:29 +0300

    .

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: "bg-accent hover:bg-blue-hover hover:scale-[0.97] shadow-button text-white",
-        back: "bg-body-accent hover:bg-body-accent/70 hover:scale-[0.97] shadow-button text-black-text"
+        back: "bg-body-accent hover:bg-body-accent/70 hover:scale-[0.97] shadow-button text-black-text text-sm max-md:text-base font-semibold"
       },
       size: {
         default: "h-10 px-6 py-6",

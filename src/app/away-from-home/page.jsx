47c7655f854cd7e531a54d53ffb39280a41a4639
mafterr11 <mafterr11@gmail.com>
2024-04-28 11:20:52 +0300

    .

--- src/app/away-from-home/page.jsx
+++ src/app/away-from-home/page.jsx
@@ -1,6 +1,6 @@
 import AwayFromHomePage from "@/components/pages/away-from-home/AwayFromHomePage"
 
-export const awayFromHomeMetadata = {
+export const metadata = {
   title: "Monte Bianco Away-From-Home - Soluții Profesionale pentru Afacerea Dumneavoastră",
   description: "Monte Bianco oferă produse de hârtie de calitate superioară pentru sectorul HORECA, clinici și hoteluri, asigurând standarde înalte de igienă și confort pentru clienții dumneavoastră.",
   keywords: "Monte Bianco HORECA, soluții igienice Monte Bianco, produse hoteluri Monte Bianco, produse clinici Monte Bianco",

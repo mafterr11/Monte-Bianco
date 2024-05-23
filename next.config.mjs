36c4033a7b85b08c8a7575f4f812023de8d58db4
mafterr11 <mafterr11@gmail.com>
2024-05-24 00:07:14 +0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // This tells Next.js to generate static files
+  trailingSlash: true, // Adding trailing slashes for better compatibility with static file servers
 };
 
 export default nextConfig;

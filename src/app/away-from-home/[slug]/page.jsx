59bab764cd3031ea610ac09a3da96084ddf60f42
Alexandru Maftei <mafterr11@gmail.com>
2024-05-23 13:50:53 +0300

    .

--- src/app/away-from-home/[slug]/page.jsx
+++ src/app/away-from-home/[slug]/page.jsx
@@ -1,11 +1,9 @@
-// app/away-from-home/[slug]/page.js (or .jsx / .ts / .tsx)
-
 import ProductPageAFHClient from './ProductPageAFHClient'; // Import the client component
 import { productData } from '@/products';
 
 export async function generateStaticParams() {
-  const paths = productData.map(product => ({
-    slug: product.slug,
+  const paths = productData.map((product) => ({
+    slug: encodeURIComponent(product.slug),
   }));
 
   return paths;
@@ -13,7 +11,8 @@ export async function generateStaticParams() {
 
 const ProductPageAFH = ({ params }) => {
   const { slug } = params;
-  const product = productData.find((product) => product.slug === decodeURIComponent(slug));
+  const decodedSlug = decodeURIComponent(slug);
+  const product = productData.find((product) => product.slug === decodedSlug);
 
   if (!product) {
     return <p>Product not found</p>;

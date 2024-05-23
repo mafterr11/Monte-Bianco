ee21db6655bbf1dab828b0991ec062a6367b7a22
Alexandru Maftei <mafterr11@gmail.com>
2024-05-23 12:53:24 +0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 // next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export', // This tells Next.js to generate static files
+    output: 'export', // This tells Next.js to generate static files
   };
   
   export default nextConfig;
--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -5,17 +5,17 @@ import { fadeIn } from "../../variants";
 
 const brands = [
   {
-    icon: "/marci/puff.png",
+    icon: "/marci/Puff.png",
     alt: "Logo marca Puff - șervețele, hârtie igienică și batiste de lux",
     fadeSpeed: 0.2,
   },
   {
-    icon: "/marci/fiore.png",
+    icon: "/marci/Fiore.png",
     alt: "Logo marca Fiore - șervețele și hârtie igienică",
     fadeSpeed: 0.4,
   },
   {
-    icon: "/marci/neve.png",
+    icon: "/marci/Neve.png",
     alt: "Logo marca Neve - role de hârtie igienică de înaltă calitate",
     fadeSpeed: 0.6,
   },

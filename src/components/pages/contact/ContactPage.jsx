c5c2cb9ac57e2ab4284ef0c3b1ba0e483d3df303
Alexandru Maftei <mafterr11@gmail.com>
2024-09-02 13:11:22 +0300

    Contact info changes

--- src/components/pages/contact/ContactPage.jsx
+++ src/components/pages/contact/ContactPage.jsx
@@ -6,10 +6,11 @@ import { fadeIn } from "../../../../variants";
 import { FiPhoneCall } from "react-icons/fi";
 import { MdOutlineFax } from "react-icons/md";
 import { SlLocationPin } from "react-icons/sl";
+import { MapPinned } from "lucide-react";
 
 const ContactPage = () => {
   return (
-    <div className="mt-40 md:max-xl:mt-28 max-md:mt-32">
+    <div className="mt-40 max-md:mt-32 md:max-xl:mt-28">
       {/* Titlu */}
       <motion.div
         variants={fadeIn("down", 0.2)}
@@ -19,8 +20,8 @@ const ContactPage = () => {
         className="text-center uppercase"
       >
         <h2>Suntem aici pentru tine</h2>
-        <h3 className="underline decoration-wavy decoration-accent underline-offset-4">
-            Nu ezita să ne contactezi
+        <h3 className="underline decoration-accent decoration-wavy underline-offset-4">
+          Nu ezita să ne contactezi
         </h3>
       </motion.div>
       {/* Mobile Panel */}
@@ -34,12 +35,11 @@ const ContactPage = () => {
           className="flex flex-wrap items-center justify-start rounded bg-body/90 py-2 text-lg md:hidden lg:bg-body"
         >
           <a
-            href="tel:+40245606296"
+            href="tel:+40722577771"
             target="blank"
             className="flex flex-col items-center justify-center gap-x-1 px-3 font-medium text-accent hover:underline hover:decoration-2 hover:underline-offset-4"
           >
-            <span className="font-semibold text-black">Tel:</span>{" "}
-            +40.245.606.296
+            <span className="font-semibold text-black">Tel:</span> +40.722.577.771
           </a>
         </motion.div>
         {/* Fax */}
@@ -93,7 +93,7 @@ const ContactPage = () => {
             montebianco@montebianco.ro
           </a>
         </motion.div>
-        {/* Adresa */}
+        {/* Sediu social */}
         <motion.div
           variants={fadeIn("down", 1.3)}
           initial="hidden"
@@ -101,13 +101,34 @@ const ContactPage = () => {
           viewport={{ once: true, ammount: 0.4 }}
           className="flex flex-col flex-wrap items-center justify-start rounded bg-body/90 px-3 py-2 text-lg md:hidden lg:bg-body"
         >
-          <h2 className="text-lg font-semibold tracking-widest">Adresa</h2>
+          <h2 className="text-lg font-semibold tracking-widest">
+            Sediu social
+          </h2>
           <a
             href="https://maps.app.goo.gl/p1wcRJjfmqQvtwxq8"
             target="_blank"
             className="hover mt-1 text-center font-medium text-accent hover:decoration-2 hover:underline-offset-4"
           >
             Strada Laminorului, numărul 16, Târgoviște, Dâmboviţa
+          </a>
+        </motion.div>
+        {/* Punct de lucru */}
+        <motion.div
+          variants={fadeIn("down", 1.6)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: true, ammount: 0.4 }}
+          className="flex flex-col flex-wrap items-center justify-start rounded bg-body/90 px-3 py-2 text-lg md:hidden lg:bg-body"
+        >
+          <h2 className="text-lg font-semibold tracking-widest">
+            Punct de lucru
+          </h2>
+          <a
+            href="https://maps.app.goo.gl/DnnwPTfXC7ykjhrz7"
+            target="_blank"
+            className="hover mt-1 text-center font-medium text-accent hover:decoration-2 hover:underline-offset-4"
+          >
+            Strada Fructelor, numărul 5, Pucioasa, Dâmbovița
           </a>
         </motion.div>
       </div>
@@ -117,9 +138,9 @@ const ContactPage = () => {
         initial="hidden"
         whileInView={"show"}
         viewport={{ once: true, ammount: 0.4 }}
-        className="container mx-auto mt-32 md:max-xl:mt-12 flex h-[900px] max-w-[1440px] flex-col-reverse gap-y-10 px-5 py-12 max-md:h-[200px] xl:pt-40"
+        className="container mx-auto mt-32 flex h-[900px] max-w-[1440px] flex-col-reverse gap-y-10 px-5 py-12 max-md:h-[200px] md:max-xl:mt-12 xl:pt-40"
       >
-        <div className="iframe-container relative z-20 flex  h-full w-full flex-col justify-end rounded-lg border-2 border-black/10 p-10 md:flex-row md:items-end md:justify-start">
+        <div className="iframe-container relative z-20 flex h-full w-full flex-col justify-end rounded-lg border-2 border-black/10 p-10 md:flex-row md:items-end md:justify-start">
           <iframe
             width="100%"
             height="100%"
@@ -128,24 +149,41 @@ const ContactPage = () => {
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2825.2685853345934!2d25.442077599999998!3d44.9178733!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b2f4190454420d%3A0x89a6aeb87ef12351!2sMONTE%20BIANCO%20S.A.!5e0!3m2!1sro!2sro!4v1713290590548!5m2!1sro!2sro"
             style={{ filter: "grayscale(0.4) contrast(1) opacity(0.7)" }}
           ></iframe>
-          {/* Adresa */}
-          <AnimatedTooltip message="Sediul nostru">
-            <div className="relative mb-4 hidden flex-wrap items-center justify-start rounded bg-body/90 p-2 shadow-button drop-shadow-lg md:flex lg:bg-body">
+          {/* Container adresa/pct de lucru */}
+          <div className="hidden flex-col items-center justify-center gap-x-5 md:flex xl:flex-row">
+            {/* Sediu social */}
+            <div className="relative mb-4 flex flex-wrap items-center justify-start rounded bg-body/90 p-2 shadow-button drop-shadow-lg lg:bg-body">
               <SlLocationPin size={31} />
               <div className="flex flex-col px-6 md:w-[350px]">
                 <h2 className="text-lg font-semibold tracking-widest">
-                  Adresa
+                  Sediu social
                 </h2>
                 <a
                   href="https://maps.app.goo.gl/p1wcRJjfmqQvtwxq8"
                   target="_blank"
-                  className="hover mt-1 text-[17px] font-medium hover:scale-[1.02] text-accent hover:decoration-2 hover:underline-offset-4"
+                  className="hover mt-1 text-[17px] font-medium text-accent hover:scale-[1.02] hover:decoration-2 hover:underline-offset-4"
                 >
                   Strada Laminorului, numărul 16, Târgoviște, Dâmboviţa
                 </a>
               </div>
             </div>
-          </AnimatedTooltip>
+            {/* Punct de lucru */}
+            <div className="relative mb-4 flex flex-wrap items-center justify-start rounded bg-body/90 p-2 shadow-button drop-shadow-lg lg:bg-body xl:ml-6">
+              <MapPinned size={33} strokeWidth={1.2} />
+              <div className="flex flex-col px-6 md:w-[350px]">
+                <h2 className="text-lg font-semibold tracking-widest">
+                  Punct de lucru
+                </h2>
+                <a
+                  href="https://maps.app.goo.gl/DnnwPTfXC7ykjhrz7"
+                  target="_blank"
+                  className="hover mt-1 text-[17px] font-medium text-accent hover:scale-[1.02] hover:decoration-2 hover:underline-offset-4"
+                >
+                  Strada Fructelor, numărul 5, Pucioasa, Dâmbovița
+                </a>
+              </div>
+            </div>
+          </div>
           <div className="absolute right-12 top-12 flex flex-col items-end justify-center gap-y-6 md:max-xl:right-4">
             {/* Telefon */}
             <AnimatedTooltip message="Haidem să discutăm!">
@@ -154,11 +192,11 @@ const ContactPage = () => {
                 <div className="flex-col flex-wrap md:flex">
                   <span className="font-semibold text-black">Tel:</span>{" "}
                   <a
-                    href="tel:+40245606296"
+                    href="tel:+40722577771"
                     target="blank"
-                    className="font-medium text-[17px] text-accent  hover:scale-[1.02] hover:underline hover:decoration-2 hover:underline-offset-4"
+                    className="text-[17px] font-medium text-accent hover:scale-[1.02] hover:underline hover:decoration-2 hover:underline-offset-4"
                   >
-                    +40.245.606.296
+                    +40.722.577.771
                   </a>
                 </div>
               </div>
@@ -172,7 +210,7 @@ const ContactPage = () => {
                   <a
                     href="tel:+40245606286"
                     target="blank"
-                    className="font-medium text-[17px] text-accent hover:scale-[1.02] hover:underline hover:decoration-2 hover:underline-offset-4"
+                    className="text-[17px] font-medium text-accent hover:scale-[1.02] hover:underline hover:decoration-2 hover:underline-offset-4"
                   >
                     +40.245.606.286
                   </a>
@@ -181,12 +219,12 @@ const ContactPage = () => {
             </AnimatedTooltip>
             {/* Vanzari */}
             <AnimatedTooltip message="Emailul departamentului de vânzări">
-              <div className="hidden flex-col  flex-wrap items-start justify-start rounded bg-body/90 px-3 py-2 text-lg shadow-button drop-shadow-lg md:flex lg:bg-body">
+              <div className="hidden flex-col flex-wrap items-start justify-start rounded bg-body/90 px-3 py-2 text-lg shadow-button drop-shadow-lg md:flex lg:bg-body">
                 <p className="font-semibold">Vânzări:</p>
                 <a
                   href="mailto: sales@montebianco.ro"
                   target="blank"
-                  className="flex items-center text-[17px] justify-center gap-x-1 font-medium text-accent hover:scale-[1.02] hover:underline hover:decoration-2 hover:underline-offset-4"
+                  className="flex items-center justify-center gap-x-1 text-[17px] font-medium text-accent hover:scale-[1.02] hover:underline hover:decoration-2 hover:underline-offset-4"
                 >
                   sales@montebianco.ro
                 </a>
@@ -194,12 +232,12 @@ const ContactPage = () => {
             </AnimatedTooltip>
             {/* Sediu */}
             <AnimatedTooltip message="Emailul sediului nostru">
-              <div className="hidden flex-col  flex-wrap items-start justify-start rounded bg-body/90 px-3 py-2 text-lg shadow-button drop-shadow-lg md:flex lg:bg-body">
+              <div className="hidden flex-col flex-wrap items-start justify-start rounded bg-body/90 px-3 py-2 text-lg shadow-button drop-shadow-lg md:flex lg:bg-body">
                 <p className="font-semibold">Sediu:</p>
                 <a
                   href="mailto: montebianco@montebianco.ro"
                   target="blank"
-                  className="flex items-center text-[17px] justify-center gap-x-1 font-medium text-accent hover:scale-[1.02] hover:underline hover:decoration-2 hover:underline-offset-4"
+                  className="flex items-center justify-center gap-x-1 text-[17px] font-medium text-accent hover:scale-[1.02] hover:underline hover:decoration-2 hover:underline-offset-4"
                 >
                   montebianco@montebianco.ro
                 </a>
